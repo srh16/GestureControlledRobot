199077ecb015719f51b5ad976de5c9456d487b8f
Sreeram Hareesh <srh16@users.noreply.github.com>
2017-04-03 20:29:40 +0530

    Update GRobo.ino

--- GRobo.ino
+++ GRobo.ino
@@ -1,60 +1,40 @@
+#include <SoftwareSerial.h>
 #define r1 9     // right motor
 #define r2 10
 #define l1 11    // left motor
 #define l2 12
-#define er 8
-#define el 13
 char str[255];
 int i=0;
-void enable()
-{
-  digitalWrite(er, HIGH);
-  digitalWrite(el, HIGH);
-}
-void disable()
-{
-   digitalWrite(er, LOW);
-   digitalWrite(el, LOW);
-}
 void forward()
 {
-   enable();
    digitalWrite(l1, HIGH);
    digitalWrite(l2, LOW);
    digitalWrite(r1, HIGH);
    digitalWrite(r2, LOW);
-   disable();
 }
 void backward()
 {
-   enable();
    digitalWrite(l1, LOW);
    digitalWrite(l2, HIGH);
    digitalWrite(r1, LOW);
    digitalWrite(r2, HIGH);
-   disable(); 
 }
 void left()
 {
-   enable();
    digitalWrite(l1, LOW);
    digitalWrite(l2, HIGH);
    digitalWrite(r1, HIGH);
    digitalWrite(r2, LOW);
-   disable();
 }
 void right()
 {   
-   enable();
    digitalWrite(l1, HIGH);
    digitalWrite(l2, LOW);
    digitalWrite(r1, LOW);
    digitalWrite(r2, HIGH);
-   disable();
 }
 void Stop()
 {
-   disable();
    digitalWrite(l1, LOW);
    digitalWrite(l2, LOW);
    digitalWrite(r1, LOW);
@@ -93,12 +73,11 @@ void setup()
 
 void loop() 
 {
-  digitalWrite(7, HIGH);
   while(Serial.available())
   {
-    //digitalWrite(7, HIGH);
     char ch=Serial.read();
     str[i++]=ch;
+    Serial.println(ch);
     test_cond(str[i-1]); 
   }
 }
